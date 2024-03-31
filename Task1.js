19dc3738d9d0248ab998aacbcf96a521ef76317f
Matthew Snow <68118415+Matthewsnow35@users.noreply.github.com>
2024-03-31 03:54:24 -0400

    Coursera Activity - Questions and Corresponding Answers

---
+++ Task1.js
@@ -0,0 +1,108 @@
+/* Exercise: Repetitive tasks with loops
+In this exercise, you will practice writing "for" and "while" loops.
+
+Task 1
+Write a "for" loop that will perform exactly the same repetitive code as this: 
+
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log('Counting completed!')
+
+Answer: */
+
+for (var i = 1; i <=5; i++) {
+    console.log(i)
+}
+console.log('Counting completed!')
+
+/*---------------------------------------------------------------------------------------------------------------------*/
+
+/* Task 2 
+Write a "for" loop that will perform exactly the same repetitive code as this: 
+
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)
+console.log('Countdown finished!')
+
+Answer: */
+
+for (var i = 5; i >= 1; i--) {
+    console.log(i)
+}
+console.log('Countdown finished!')
+
+/*---------------------------------------------------------------------------------------------------------------------*/
+
+/* Task 3
+Write a "while" loop that will perform exactly the same repetitive code as this:
+
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log('Counting completed!')
+
+Answer: */
+
+var i = 1
+
+while  (i <= 5) {
+    console.log(i)
+    i++
+}
+console.log('Counting Completed!')
+
+/*---------------------------------------------------------------------------------------------------------------------*/
+
+/* Task 4
+Write a "while" loop that will perform exactly the same repetitive code as this:
+
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)
+console.log('Countdown finished!')
+
+Answer: */
+
+
+var i = 5
+
+while (i >= 1) {
+    console.log(i)
+    i = i - 1;
+}
+console.log('Countdown finished!')
+
+/*---------------------------------------------------------------------------------------------------------------------*/
+
+/* Task 5
+Write a "while" loop that will perform exactly the same repetitive code as this:
+
+console.log(2018)
+console.log(2019)
+console.log(2020)
+console.log(2021)
+console.log(2022)
+
+Answer: */
+
+
+var i = 2018
+
+while (i <= 2022) {
+    console.log(i)
+    i++
+}
+
+
+
+
